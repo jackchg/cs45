fb9d268e6736721c43a7931b928e529675b0e197
Jackie Gong <gongjackiegong@gmail.com>
2023-02-12 17:32:11 -0800

    Attemping to rematch on ties
    Stopped reprint of instructions on rematch

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,5 +1,20 @@
 import random
 
+def rps():
+  player_selection = input("What do you select? (rock, paper, scissors): ")
+  selections = ["rock", "paper", "scissors"]
+  computer_selection = random.choice(selections)
+  p_idx = selections.index(player_selection)
+  c_idx = selections.index(computer_selection)
+  if p_idx == c_idx:
+    print(f"You both selected {player_selection}. It's a tie!")
+    print("\nThe computer wants a rematch!\n")
+    rps()
+  elif p_idx == c_idx + 1 or (p_idx == 0 and c_idx == 2):
+    print(f'You selected {player_selection}. The computer selected {computer_selection}. You win!')
+  else:
+    print(f'You selected {player_selection}. The computer selected {computer_selection}. You lose...')
+
 def main():
   print("Hello, welcome to Rock, Paper, Scissors!")
   print("Here are the rules:")
@@ -10,17 +25,8 @@ def main():
    beats paper (by cutting it); and paper beats rock (by covering it). If both players
    select the same one, it is a tie.
   """)
-  player_selection = input("What do you select? (rock, paper, scissors): ")
-  selections = ["rock", "paper", "scissors"]
-  computer_selection = random.choice(selections)
-  p_idx = selections.index(player_selection)
-  c_idx = selections.index(computer_selection)
-  if p_idx == c_idx:
-    print(f"You both selected {player_selection}. It's a tie!")
-  elif p_idx == c_idx + 1 or (p_idx == 0 and c_idx == 2):
-    print(f'You selected {player_selection}. The computer selected {computer_selection}. You win!')
-  else:
-    print(f'You selected {player_selection}. The computer selected {computer_selection}. You lose...')
+  rps()
 
 if __name__ == '__main__':
   main()
+
