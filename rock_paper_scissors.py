e33865862fae69a263900ee836d5559d0231bfee
Jackie Gong <gongjackiegong@gmail.com>
2023-02-12 17:05:00 -0800

    Added explanation of rules for RPS

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,5 +1,14 @@
 def main():
   print("Hello, welcome to Rock, Paper, Scissors!")
+  print("Here are the rules:")
+  print("""
+   Two players secretly pick one of “rock,” “paper,” or “scissors.”
+   Both players reveal their selection to the other player at once; the winner is chosen
+   based on what the selections are. Rock beats scissors (by crushing them); scissors
+   beats paper (by cutting it); and paper beats rock (by covering it). If both players
+   select the same one, it is a tie.
+  """)
+
 
 if __name__ == '__main__':
   main()
